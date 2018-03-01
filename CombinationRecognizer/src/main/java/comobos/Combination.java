fe339e7d186e977b2e1905d7632beb746f4b09a3
Sitora <redastify@gmail.com>
2018-03-01 20:44:47 +0300

    исправление небольших ошибок

---
+++ CombinationRecognizer/src/main/java/comobos/Combination.java
@@ -0,0 +1,18 @@
+package comobos;
+import
+
+public interface Combination {
+    private final Integer POWER;
+
+    public Combination(int power) {
+
+    }
+
+    public Combination(Hand hand, Board board) {
+
+    }
+
+    public static boolean isThisCombination() {
+        return false;
+    }
+}
